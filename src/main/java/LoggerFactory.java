0e84d4805c2c86c6d9a932075f5b407ce9396988
anmol2302 <anmolgupta23021997@gmail.com>
2019-05-24 11:10:27 +0530

    Initail Commit

---
+++ src/main/java/LoggerFactory.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+
+public class LoggerFactory {
+    public static Logger getLoggerInstance(String tag) {
+        return Logger.getLogger(tag);
+
+    }
+
+    private LoggerFactory() {
+    }
+
+
+
+
+}
+
